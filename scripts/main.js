61f10f9fd8e75a704aa6dfd3a19c8904d65f8adf
Mark Ioannidis <mark.ioannidis@gmail.com>
2017-01-24 23:12:09 -0500

    adding initial javascript scroll functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
-document.getElementById('container').onclick = function(e) {
-    move('.topNav')
-    .set('opacity', 0)
-    .end();
-};
+var topNavScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 800) { 
+			move('.topNav')
+    	.set('opacity', 1)
+    	.end();
+    	move('.pane1Content')
+    	.set('opacity', 0)
+    	.end(); }
+    else {
+    	move('.topNav')
+    	.set('opacity', 0)
+    	.end();
+    	move('.pane1Content')
+    	.set('opacity', 1)
+    	.end();
+    }
+}
 
-document.getElementById('container').onclick = function(e) {
+window.addEventListener('scroll', topNavScrollFunc);
+
+document.getElementsByClassName('pane1').onclick = function(e) {
   	move('.pane1')
     .set('opacity', 0)
     .end();
