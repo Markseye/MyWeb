1378d7776f0a14db436a6e20a11e2a843ef61bfa
Mark Ioannidis <mark.ioannidis@gmail.com>
2017-01-27 20:12:07 -0500

    updating responsive through css and removing unneeded js

--- scripts/main.js
+++ scripts/main.js
@@ -19,36 +19,4 @@ var topNavScrollFunc = function () {
 
 window.addEventListener('scroll', topNavScrollFunc);
 
-var responsivePanes = function () {
-		var x = window.innerWidth;
-		if (x < 800) {
-			move('#container')
-			.set('flex-direction', 'column')
-			.end();
-			move('.pane2')
-			.set('flex-direction', 'row')
-			.set('margin', 0)
-			.set('justify-content', 'center')
-			.set('width', 'inherit')
-			.end();
-			move('.pane1')
-			.set('position', 'initial')
-			.set('width', '100%')
-			.set('height', '100vh')
-			.end();
-			move('.setext')
-			.set('padding-top', '10%')
-			.end();
-		}
-};
-
-window.onload = responsivePanes;
-
-document.getElementsByClassName('pane1').onclick = function(e) {
-  	move('.pane1')
-    .set('opacity', 0)
-    .end();
-};
-
-
 //add listener for if width is less to 900px to move panes to wrap
