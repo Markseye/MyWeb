629af60958ca7c7b38a7f2e0c598662e8bcaa529
Mark Ioannidis <mark.ioannidis@gmail.com>
2017-02-02 00:04:57 -0500

    updating top nav and link classes, removing old js

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,23 @@ var topNavScrollFunc = function () {
     }
 };
 
+/*var topNavScrollFunc = function () {
+    var y = window.scrollY;
+    var tnav = document.getElementsByClassName("topNav")
+    var p1c = document.getElementsByClassName("pane1Content")
+    if (y >= 800) { 
+			p1c.parentNode.removeChild(pane1content)
+		}
+    else {
+    	move('.topNav')
+    	.set('opacity', 0)
+    	.end();
+    	move('.pane1Content')
+    	.set('opacity', 1)
+    	.end();
+    }
+};*/
+
 window.addEventListener('scroll', topNavScrollFunc);
 
 //add listener for if width is less to 900px to move panes to wrap
